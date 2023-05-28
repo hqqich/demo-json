e43ac5d25659b43aa154a304cfea57695df17b65
hqqich <hqqich1314@outlook.com>
2023-05-28 16:04:53 +0800

    修改

--- gson/src/test/java/org/example/AppTest.java
+++ gson/src/test/java/org/example/AppTest.java
@@ -1,38 +1,45 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.example.entity.School;
+import org.example.util.SchoolUtil;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+
+    @Test
+    public void test01() {
+
+        Gson gson = new Gson();
+
+        School schoolData = SchoolUtil.getSchoolData();
+
+        //JSONObject info = new JSONObject();
+        //info.put("name", "张三");
+        //info.put("age", "18");
+        //info.put("地理", "70");
+        //info.put("英语", "60");
+
+        //JSON对象转字符串
+        String str = gson.toJson(schoolData);
+        //JSON字符串转JSON对象
+        School school = gson.fromJson(str, School.class);
+
+
+
+        JsonElement jsonElement = gson.toJsonTree(str);
+        JsonObject asJsonObject = gson.fromJson(str, JsonObject.class);
+        long id = asJsonObject.get("id").getAsLong();
+
+        System.out.println(school);
+
+
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
